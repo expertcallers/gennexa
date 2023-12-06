f03c92dd12d450768027c86cdaee75f6f5b73580
JoshuaMyers <j.myers@expertcallers.com>
2023-12-06 17:50:28 +0530

    updated prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
+  site: 'https://expertcallers.github.io',
+  base: '/gennexa',
   build: {
-    assets: "static"
+    assets: "static",
   },
   integrations: [tailwind(), mdx()]
 });
