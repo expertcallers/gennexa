3ec2d952885fdce8f64a3e783ea692f926e2e6d2
lohit <lohit.n@expertcallers.com>
2023-12-06 20:00:27 +0530

    updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
-  site: 'https://expertcallers.github.io',
-  base: '/gennexa',
+  site: 'https://www.gennexa.ai',
   build: {
     assets: "static",
   },
